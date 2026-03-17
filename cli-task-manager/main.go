fe4a942a2c94ab44109eba8a895293d5e36f3da6
Alexandr <sanoktabx@gmail.com>
2026-03-17 06:56:05 +0500

    Added json file

--- cli-task-manager/main.go
+++ cli-task-manager/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -13,13 +15,41 @@ type Task struct {
 
 func main() {
 
-	var tasks []Task
-
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: task-manager <command> [args]")
 		return
 	}
 
+	tasks, err := loadTasks()
+
+	if err != nil {
+		fmt.Println("Failed to load tasks")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: task-manager add <text>")
+			return
+		}
+
+		text := os.Args[2]
+		tasks = addTask(tasks, text)
+
+		if err := saveTasks(tasks); err != nil {
+			fmt.Println("Failed to save tasks")
+			return
+		}
+
+		fmt.Printf("Added task %d: %s\n", len(tasks), text)
+	case "list":
+		printTasks(tasks)
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+	}
 }
 
 func addTask(tasks []Task, text string) []Task {
@@ -56,4 +86,33 @@ func printTasks(tasks []Task) {
 		}
 		fmt.Printf("[%s] %d %s\n", check, tasks[i].ID, tasks[i].Text)
 	}
+}
+
+func saveTasks(tasks []Task) error {
+	data, err := json.Marshal(tasks)
+
+	if err != nil {
+		return err
+	}
+
+	os.WriteFile("tasks.json", data, 0644)
+
+	return nil
+}
+
+func loadTasks() ([]Task, error) {
+	var loaded []Task
+	data, err := os.ReadFile("tasks.json")
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return []Task{}, nil
+		}
+
+		return nil, err
+	}
+
+	json.Unmarshal(data, &loaded)
+
+	return loaded, nil
 }
